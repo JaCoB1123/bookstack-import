86e8c5f8f60db07dce608c054140a71ef0d61773
Jan Bader <jan.bader@ub-software.de>
2022-05-23 12:53:19 +0200

    Use WalkDir and implement creation and updating of page-contents

--- main.go
+++ main.go
@@ -1,12 +1,109 @@
 package main
 
 import (
+	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/exp/slices"
 )
+
+type bookstackImport struct {
+	Client   *bookStackClient
+	Books    []book
+	Chapters []chapter
+	Pages    []page
+}
+
+func NewImport(client *bookStackClient) *bookstackImport {
+	imp := &bookstackImport{
+		Client: client,
+	}
+	books, err := client.GetBooks()
+	if err != nil {
+		log.Fatal("Could not get list of books:", err)
+		return nil
+	}
+	imp.Books = books.Data
+
+	chapters, err := client.GetChapters()
+	if err != nil {
+		log.Fatal("Could not get list of chapters:", err)
+		return nil
+	}
+	imp.Chapters = chapters.Data
+
+	pages, err := client.GetPages()
+	if err != nil {
+		log.Fatal("Could not get list of pages:", err)
+		return nil
+	}
+	imp.Pages = pages.Data
+	return imp
+}
+
+func (imp bookstackImport) GetBook(name string) *book {
+	matchingBookIndex := slices.IndexFunc(imp.Books, func(b book) bool { return b.Name == name })
+	if matchingBookIndex == -1 {
+		log.Println("Creating new book", name)
+		newBook, err := imp.Client.CreateBook(name)
+		if err != nil {
+			log.Fatal(err)
+			return nil
+		}
+
+		matchingBookIndex = len(imp.Books)
+		imp.Books = append(imp.Books, *newBook)
+		log.Println("New book:", newBook)
+	}
+	return &imp.Books[matchingBookIndex]
+}
+
+func (imp bookstackImport) GetChapter(name string, bookID int) *chapter {
+	matchingChapterIndex := slices.IndexFunc(imp.Chapters, func(b chapter) bool { return b.Name == name && b.BookID == bookID })
+	if matchingChapterIndex == -1 {
+		log.Println("Creating new chapter", name)
+		newChapter, err := imp.Client.CreateChapter(bookID, name)
+		if err != nil {
+			log.Fatal(err)
+			return nil
+		}
+
+		matchingChapterIndex = len(imp.Chapters)
+		imp.Chapters = append(imp.Chapters, *newChapter)
+		log.Println("New chapter:", newChapter)
+	}
+
+	return &imp.Chapters[matchingChapterIndex]
+}
+
+func (imp bookstackImport) GetPage(name string, bookID int, chapterID int, content []byte) *page {
+	matchingPageIndex := slices.IndexFunc(imp.Pages, func(b page) bool {
+		return b.Name == name && b.BookID == bookID && b.ChapterID == chapterID
+	})
+	if matchingPageIndex == -1 {
+		log.Println("Creating new page", name)
+		newPage, err := imp.Client.CreatePage(bookID, chapterID, name, content)
+		if err != nil {
+			return nil
+		}
+
+		imp.Pages = append(imp.Pages, *newPage)
+		return newPage
+	}
+
+	existingPage := &imp.Pages[matchingPageIndex]
+	page, err := imp.Client.UpdatePageContent(existingPage.ID, content)
+	if err != nil {
+		log.Printf("could not update page %d: %s\n", page.ID, err)
+		return nil
+	}
+	imp.Pages[matchingPageIndex] = *page
+	return page
+}
 
 func main() {
 	url := os.Getenv("BOOKSTACK_URL")
@@ -19,106 +116,37 @@ func main() {
 		panic(err)
 	}
 
-	bookDirectories, err := os.ReadDir(importPath)
-	if err != nil {
-		log.Fatal("Could not get files from "+importPath, err)
-		return
-	}
+	imp := NewImport(client)
 
-	books, err := client.GetBooks()
-	if err != nil {
-		log.Fatal("Could not get list of books:", err)
-		return
-	}
-
-	chapters, err := client.GetChapters()
-	if err != nil {
-		log.Fatal("Could not get list of chapters:", err)
-		return
-	}
-
-	pages, err := client.GetPages()
-	if err != nil {
-		log.Fatal("Could not get list of pages:", err)
-		return
-	}
-
-	for _, bookDirectory := range bookDirectories {
-		matchingBookIndex := slices.IndexFunc(books.Data, func(b book) bool { return b.Name == bookDirectory.Name() })
-		if matchingBookIndex == -1 {
-			log.Println("Creating new book", bookDirectory.Name())
-			newBook, err := client.CreateBook(bookDirectory.Name())
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-
-			matchingBookIndex = len(books.Data)
-			books.Data = append(books.Data, *newBook)
-			log.Println("New book:", newBook)
-		}
-
-		matchingBook := books.Data[matchingBookIndex]
-
-		importPath := filepath.Join(importPath, bookDirectory.Name())
-		chapterDirectories, err := os.ReadDir(importPath)
+	err = filepath.WalkDir(importPath, func(fullPath string, info fs.DirEntry, err error) error {
 		if err != nil {
-			log.Fatal("Could not get files from "+importPath, err)
-			return
+			return err
 		}
 
-		for _, chapterDirectory := range chapterDirectories {
-			matchingChapterIndex := slices.IndexFunc(chapters.Data, func(b chapter) bool { return b.Name == chapterDirectory.Name() && b.BookID == matchingBook.ID })
-			if matchingChapterIndex == -1 {
-				log.Println("Creating new chapter", chapterDirectory.Name())
-				newChapter, err := client.CreateChapter(matchingBook.ID, chapterDirectory.Name())
-				if err != nil {
-					log.Fatal(err)
-					return
-				}
-
-				matchingChapterIndex = len(chapters.Data)
-				chapters.Data = append(chapters.Data, *newChapter)
-				log.Println("New chapter:", newChapter)
-			}
-
-			matchingChapter := chapters.Data[matchingChapterIndex]
-
-			importPath := filepath.Join(importPath, chapterDirectory.Name())
-
-			// from here, handle leafs only, as the nesting is limited
-			err := filepath.Walk(importPath,
-				func(path string, info os.FileInfo, err error) error {
-					if err != nil {
-						return err
-					}
-
-					if info.IsDir() {
-						return nil
-					}
-
-					matchingPageIndex := slices.IndexFunc(pages.Data, func(b page) bool {
-						return b.Name == chapterDirectory.Name() && b.BookID == matchingBook.ID && b.ChapterID == matchingChapter.ID
-					})
-					if matchingPageIndex == -1 {
-						log.Println("Creating new page", chapterDirectory.Name())
-						newPage, err := client.CreatePage(matchingBook.ID, matchingChapter.ID, chapterDirectory.Name())
-						if err != nil {
-							return err
-						}
-
-						matchingPageIndex = len(pages.Data)
-						pages.Data = append(pages.Data, *newPage)
-						log.Println("New page:", newPage)
-					}
-					return nil
-				})
-			if err != nil {
-				panic(err)
-			}
-			return
+		if info.IsDir() {
+			return nil
 		}
 
-		return
+		path := fullPath[len(importPath)+1:]
+
+		segments := strings.Split(path, "\\")
+
+		log.Println(path)
+
+		book := imp.GetBook(segments[0])
+		chapter := imp.GetChapter(segments[1], book.ID)
+		pageName := strings.Join(segments[2:], "\\")
+
+		content, err := ioutil.ReadFile(fullPath)
+		if err != nil {
+			return err
+		}
+
+		imp.GetPage(pageName, book.ID, chapter.ID, content)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
 	}
 }
--- pages.go
+++ pages.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 type pagesResponse struct {
 	Data []page `json:"data"`
@@ -13,6 +17,7 @@ type page struct {
 	Name        string `json:"name"`
 	Slug        string `json:"slug"`
 	Description string `json:"description"`
+	Markdown    string `json:"markdown"`
 }
 
 func (client bookStackClient) GetPages() (*pagesResponse, error) {
@@ -26,16 +31,35 @@ func (client bookStackClient) GetPages() (*pagesResponse, error) {
 	return resp.Result().(*pagesResponse), nil
 }
 
-func (client bookStackClient) CreatePage(bookID, chapterID int, name string) (*page, error) {
+func (client bookStackClient) CreatePage(bookID, chapterID int, name string, content []byte) (*page, error) {
 	resp, err := client.R().
 		SetBody(page{
 			BookID:    bookID,
 			ChapterID: chapterID,
-			Name:      name}).
+			Name:      name,
+			Markdown:  string(content)}).
 		SetResult(page{}).
 		Post("/api/pages")
 	if err != nil {
 		return nil, fmt.Errorf("create page: %w", err)
 	}
+	log.Println(resp)
+	return resp.Result().(*page), nil
+}
+
+type pageContentRequest struct {
+	Markdown string `json:"markdown"`
+}
+
+func (client bookStackClient) UpdatePageContent(pageID int, content []byte) (*page, error) {
+	resp, err := client.R().
+		SetBody(pageContentRequest{
+			Markdown: string(content)}).
+		SetResult(page{}).
+		Put("/api/pages/" + strconv.Itoa(pageID))
+	if err != nil {
+		return nil, fmt.Errorf("update page: %w", err)
+	}
+	log.Println(resp)
 	return resp.Result().(*page), nil
 }
